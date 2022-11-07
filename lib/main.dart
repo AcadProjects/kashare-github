18d485c4cc65587492afac14ba16a251d70fb5e3
Harold Simeon <harodtsimeon@gmail.com>
2022-11-07 21:11:03 +0800

    Added MapBox Widget to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,3 +17,16 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: SfMaps(
+      layers: [
+        MapTileLayer(
+          urlTemplate:'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5naGVseiIsImEiOiJjbDd2anV6NHUwYzh4M3ZveHdudGQ0dHU1In0.m5vUkL5mWj1JxmojY55ZCg', 
+        ),
+      ],
+    ),
+  );
+}
