fed8e23d9236240182f9580af779f2fe2730a8fb
Harold Simeon <harodtsimeon@gmail.com>
2022-11-11 19:53:48 +0800

    Fixed User class

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,4 @@
-//import 'package:kashare/models/user.dart';
+import 'package:kashare/models/myuser.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
@@ -6,16 +6,16 @@ class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   // Create user object based on firebase user
-  User _userFromFirebaseUser(User user) {
-    return user != null ? User(uid: user.uid) : null;
+  MyUser? _userFromFirebaseUser(User user) {
+    return user != null ? MyUser(uid: user.uid) : null;
   }
 
   // Sign In Anonymously
   Future signInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
-      User user = result.user;
-      return _userFromFirebaseUser(user);
+      User? user = result.user;
+      return _userFromFirebaseUser(user!);
     } catch (e) {
       print(e.toString());
       return null;
