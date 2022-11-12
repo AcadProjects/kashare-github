91ffe20464f9fdc19ef338e884dd853eb9dea522
Harold Simeon <harodtsimeon@gmail.com>
2022-11-13 06:32:42 +0800

    reverted sign in

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:kashare/services/auth.dart';
+import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
   final toggleView;
-  SignIn({this.toggleView});
-  //const SignIn({Key? key, this.toggleView}) : super(key: key);
-
+  //SignIn({this.toggleView});
+  const SignIn({Key? key, this.toggleView}) : super(key: key);
   @override
   _SignInState createState() => _SignInState();
 }
@@ -15,17 +14,18 @@ class _SignInState extends State<SignIn> {
   final _formKey = GlobalKey<FormState>();
   String error = '';
 
+  // text field state
   String email = '';
   String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[300],
+      backgroundColor: Colors.green[100],
       appBar: AppBar(
         backgroundColor: Colors.green[400],
         elevation: 0.0,
-        title: Text('Sign In to KaShare'),
+        title: Text('Sign in to Brew Crew'),
         actions: <Widget>[
           ElevatedButton.icon(
             icon: Icon(Icons.person),
@@ -37,8 +37,9 @@ class _SignInState extends State<SignIn> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
-            key: _formKey,
-            child: Column(children: <Widget>[
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
               SizedBox(height: 20.0),
               TextFormField(
                 validator: (val) => val!.isEmpty ? 'Enter an email' : null,
@@ -48,49 +49,46 @@ class _SignInState extends State<SignIn> {
               ),
               SizedBox(height: 20.0),
               TextFormField(
-                  obscureText: true,
-                  validator: (val) => val!.length < 7
-                      ? 'Enter a password not less than 7 chars long'
-                      : null,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  }),
-
+                obscureText: true,
+                validator: (val) =>
+                    val!.length < 7 ? 'Enter a password 7 chars long' : null,
+                onChanged: (val) {
+                  setState(() => password = val.trim());
+                },
+              ),
               SizedBox(height: 20.0),
               ElevatedButton(
-                //color: Colors.green[300],
-                child: Text(
-                  'Sign In',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () async {
-                 //print(email);
-                 // print(password);
-                   if(_formKey.currentState!.validate()){
-                    dynamic result = await _auth.signInWithEmailAndPassword(email, password);
-                    if(result == null) {
-                      setState(() {
-                        error = 'Could not sign in with those credentials';
-                      });
+                  // color: Colors.pink[400],
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      dynamic result = await _auth.signinWithEmailAndPassword(
+                          email, password);
+                      if (result == null) {
+                        setState(() {
+                          error = 'Could not sign in with those credentials';
+                        });
+                      }
                     }
-                  }
-                }
-              ),
+                  }),
               SizedBox(height: 12.0),
               Text(
                 error,
                 style: TextStyle(color: Colors.red, fontSize: 14.0),
               ),
-
-        SizedBox(height: 20.0),     
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.green,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(100.0),
-            ),
+            ],
           ),
-          child: Text('Sign In Anonymously'),
+        ),
+      ),
+    );
+  }
+}
+
+/*
+child: Text('Sign In Anonymously'),
           onPressed: () async {
             dynamic result = await _auth.signInAnon();
             if (result == null) {
@@ -100,8 +98,4 @@ class _SignInState extends State<SignIn> {
               print(result.uid);
             }
           },
-  ),
-  ]
-  ));)
-
-
+*/
