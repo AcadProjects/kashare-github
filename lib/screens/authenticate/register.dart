96fd31b656430c0ea15f9b33379601354987f989
Harold Simeon <harodtsimeon@gmail.com>
2022-11-13 13:39:35 +0800

    Added animations

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,5 +1,7 @@
 import 'package:kashare/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:kashare/shared/constants.dart';
+import 'package:kashare/shared/loading.dart';
 
 class Register extends StatefulWidget {
   final toggleView;
@@ -13,7 +15,7 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
-
+  bool loading = false;
   // text field state
   String email = '';
   String password = '';
@@ -21,183 +23,143 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green[200],
-      appBar: AppBar(
-        backgroundColor: Colors.green[400],
-        elevation: 0.0,
-        title: Text('Sign up to KaShare'),
-        actions: <Widget>[
-          ElevatedButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Sign In'),
-            onPressed: () => widget.toggleView(),
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-//
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'First Name',
-                  fillColor: Colors.white,
-                  filled: true,
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white, width: 2.0)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 2.0)),
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.green[200],
+            appBar: AppBar(
+              backgroundColor: Colors.green[400],
+              elevation: 0.0,
+              title: Text('Sign up to KaShare'),
+              actions: <Widget>[
+                ElevatedButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text('Sign In'),
+                  onPressed: () => widget.toggleView(),
                 ),
-                validator: (val) => val!.isEmpty ? 'Enter First Name' : null,
-                onChanged: (val) {
-                  setState(() => email = val.trim());
-                },
-              ), //TextFormField - First Name
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
 //
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Last Name',
-                  fillColor: Colors.white,
-                  filled: true,
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white, width: 2.0)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 2.0)),
-                ),
-                validator: (val) => val!.isEmpty ? 'Enter Last Name' : null,
-                onChanged: (val) {
-                  setState(() => email = val.trim());
-                },
-              ), //TextFormField - Last Name
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'First Name'),
+                      validator: (val) =>
+                          val!.isEmpty ? 'Enter First Name' : null,
+                      onChanged: (val) {
+                        setState(() => email = val.trim());
+                      },
+                    ), //TextFormField - First Name
+//
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Last Name'),
+                      validator: (val) =>
+                          val!.isEmpty ? 'Enter Last Name' : null,
+                      onChanged: (val) {
+                        setState(() => email = val.trim());
+                      },
+                    ), //TextFormField - Last Name
 //
 
 //
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                  fillColor: Colors.white,
-                  filled: true,
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white, width: 2.0)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 2.0)),
-                ),
-                validator: (val) => val!.isEmpty ? 'Enter your email' : null,
-                onChanged: (val) {
-                  setState(() => email = val.trim());
-                },
-              ), //TextFormField - Email
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Email'),
+                      validator: (val) =>
+                          val!.isEmpty ? 'Enter your email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val.trim());
+                      },
+                    ), //TextFormField - Email
 //
 
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Address',
-                  fillColor: Colors.white,
-                  filled: true,
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white, width: 2.0)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 2.0)),
-                ),
-                validator: (val) => val!.isEmpty ? 'Enter your Address' : null,
-                onChanged: (val) {
-                  setState(() => email = val.trim());
-                },
-              ), //TextFormField - Address
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Address'),
+                      validator: (val) =>
+                          val!.isEmpty ? 'Enter your Address' : null,
+                      onChanged: (val) {
+                        setState(() => email = val.trim());
+                      },
+                    ), //TextFormField - Address
 //
 
-              SizedBox(height: 20.0),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  hintText: 'Phone Number',
-                  fillColor: Colors.white,
-                  filled: true,
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white, width: 2.0)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 2.0)),
-                ),
-                validator: (val) =>
-                    val!.isEmpty ? 'Enter your Phone Number' : null,
-                onChanged: (val) {
-                  setState(() => email = val.trim());
-                },
-              ), //TextFormField - Phone Number
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: textInputDecoration.copyWith(
+                          hintText: 'Phone Number'),
+                      validator: (val) =>
+                          val!.isEmpty ? 'Enter your Phone Number' : null,
+                      onChanged: (val) {
+                        setState(() => email = val.trim());
+                      },
+                    ), //TextFormField - Phone Number
 //
-              SizedBox(height: 40.0),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Password'),
+                      obscureText: true,
+                      validator: (val) => val!.length < 7
+                          ? 'Enter a password (Must be greater than 7 characters)'
+                          : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ), //TextFormField - Password
 
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  fillColor: Colors.white,
-                  filled: true,
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white, width: 2.0)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 2.0)),
+                    TextFormField(
+                      decoration: textInputDecoration.copyWith(
+                          hintText: 'Confirm Password'),
+                      obscureText: true,
+                      validator: (val) =>
+                          val!.length < 7 ? 'Confirm Password' : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ), //TextFormField - Confirm Password
+
+                    SizedBox(height: 20.0),
+                    ElevatedButton(
+                        // Colors.green[400],
+                        child: Text(
+                          'Register',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            // print(email);
+                            // print(password);
+                            setState(() => loading = true);
+                            dynamic result = await _auth
+                                .registerWithEmailAndPassword(email, password);
+                            if (result == null) {
+                              setState(() => error = 'Enter valid email');
+                              loading = false;
+                            }
+                          } else {}
+
+                          //
+                          //
+                        }),
+                    SizedBox(height: 12.0),
+                    Text(error,
+                        style: TextStyle(color: Colors.red, fontSize: 14.0)),
+                  ],
                 ),
-                obscureText: true,
-                validator: (val) => val!.length < 7
-                    ? 'Enter a password (Must be greater than 7 characters)'
-                    : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ), //TextFormField - Password
-
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Confirm Password',
-                  fillColor: Colors.white,
-                  filled: true,
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white, width: 2.0)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 2.0)),
-                ),
-                obscureText: true,
-                validator: (val) => val!.length < 7 ? 'Confirm Password' : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ), //TextFormField - Confirm Password
-
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                  // Colors.green[400],
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      // print(email);
-                      // print(password);
-                      dynamic result = await _auth.registerWithEmailAndPassword(
-                          email, password);
-                      if (result == null) {
-                        setState(() => error = 'Enter valid email');
-                      }
-                    } else {}
-
-                    //
-                    //
-                  }),
-              SizedBox(height: 12.0),
-              Text(error, style: TextStyle(color: Colors.red, fontSize: 14.0)),
-            ],
-          ),
-        ),
-      ),
-    );
+              ),
+            ),
+          );
   }
 }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:kashare/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:kashare/shared/loading.dart';
 
 class SignIn extends StatefulWidget {
   final toggleView;
@@ -13,108 +14,119 @@ class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
   String error = '';
-
+  bool loading = false;
   // text field state
   String email = '';
   String password = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green[100],
-      appBar: AppBar(
-        backgroundColor: Colors.green[400],
-        elevation: 0.0,
-        title: Text('Sign in to KaShare'),
-        actions: <Widget>[
-          ElevatedButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Register'),
-            onPressed: () => widget.toggleView(),
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                  fillColor: Colors.white,
-                  filled: true,
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white, width: 2.0)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 2.0)),
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.green[100],
+            appBar: AppBar(
+              backgroundColor: Colors.green[400],
+              elevation: 0.0,
+              title: Text('Sign in to KaShare'),
+              actions: <Widget>[
+                ElevatedButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text('Register'),
+                  onPressed: () => widget.toggleView(),
                 ),
-                validator: (val) => val!.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val.trim());
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  fillColor: Colors.white,
-                  filled: true,
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white, width: 2.0)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey, width: 2.0)),
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Email',
+                        fillColor: Colors.white,
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 2.0)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey, width: 2.0)),
+                      ),
+                      validator: (val) =>
+                          val!.isEmpty ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val.trim());
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        fillColor: Colors.white,
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 2.0)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey, width: 2.0)),
+                      ),
+                      obscureText: true,
+                      validator: (val) => val!.length < 7
+                          ? 'Enter a password 7 chars long'
+                          : null,
+                      onChanged: (val) {
+                        setState(() => password = val.trim());
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    ElevatedButton(
+                        // color: Colors.pink[400],
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _auth
+                                .signInWithEmailAndPassword(email, password);
+                            if (result == null) {
+                              setState(() {
+                                error =
+                                    'Could not sign in with those credentials';
+                                loading = false;
+                              });
+                            }
+                          }
+                        }),
+                    SizedBox(height: 12.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    ),
+                    SizedBox(height: 20.0),
+                    ElevatedButton(
+                      child: Text('Sign In Using Google'),
+                      onPressed: () async {
+                        dynamic result = await _auth.signInAnon();
+                        if (result == null) {
+                          print('Error Signing In');
+                        } else {
+                          print('Signed In');
+                          print(result.uid);
+                        }
+                      },
+                    )
+                  ],
                 ),
-                obscureText: true,
-                validator: (val) =>
-                    val!.length < 7 ? 'Enter a password 7 chars long' : null,
-                onChanged: (val) {
-                  setState(() => password = val.trim());
-                },
               ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                  // color: Colors.pink[400],
-                  child: Text(
-                    'Sign In',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      dynamic result = await _auth.signInWithEmailAndPassword(
-                          email, password);
-                      if (result == null) {
-                        setState(() {
-                          error = 'Could not sign in with those credentials';
-                        });
-                      }
-                    }
-                  }),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                child: Text('Sign In Using Google'),
-                onPressed: () async {
-                  dynamic result = await _auth.signInAnon();
-                  if (result == null) {
-                    print('Error Signing In');
-                  } else {
-                    print('Signed In');
-                    print(result.uid);
-                  }
-                },
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
 
