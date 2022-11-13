dd8b392cf844a7e0cd45196b794ed1e2ff1e7610
Harold Simeon <harodtsimeon@gmail.com>
2022-11-13 16:27:29 +0800

    Added null exception for datbase values

--- lib/models/kashare.dart
+++ lib/models/kashare.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Kashare {
-  final String name;
-  final String location;
-  final String destination;
-  final int lat;
-  final int long;
+  //const Kashare({Key? key}) : super(key: key);
+  final String? name;
+  final String? location;
+  final String? destination;
+  final int? lat;
+  final int? long;
 
   Kashare({this.name, this.location, this.destination, this.lat, this.long});
 }
